5f6d9e3be3ec0fe7186edd1730071e0293b316c9
lucky7tb <luckytri101@gmail.com>
2025-01-11 11:32:00 +0700

    comment fallback

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,19 +8,20 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        web: __DIR__ . "/../routes/web.php",
+        api: __DIR__ . "/../routes/api.php",
+        commands: __DIR__ . "/../routes/console.php",
+        health: "/up"
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $e) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'not found',
-            ], Response::HTTP_NOT_FOUND);
-        });
-    })->create();
+        // $exceptions->render(function (NotFoundHttpException $e) {
+        //     return response()->json([
+        //         'status' => 'error',
+        //         'message' => 'not found',
+        //     ], Response::HTTP_NOT_FOUND);
+        // });
+    })
+    ->create();
--- routes/api.php
+++ routes/api.php
@@ -3,34 +3,58 @@
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-Route::get('/', function () {
-    return response()->json([
-        'status' => 'success',
-        'message' => 'welcome!',
-    ], Response::HTTP_OK);
+Route::get("/", function () {
+    return response()->json(
+        [
+            "status" => "success",
+            "message" => "welcome!",
+        ],
+        Response::HTTP_OK
+    );
 });
 
-Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post("login", [\App\Http\Controllers\AuthController::class, "login"]);
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+Route::middleware(["auth:sanctum"])->group(function () {
+    Route::post("logout", [
+        \App\Http\Controllers\AuthController::class,
+        "logout",
+    ]);
 
-    Route::get('divisions', \App\Http\Controllers\DivisionController::class);
+    Route::get("divisions", \App\Http\Controllers\DivisionController::class);
 
-    Route::prefix('employees')->group(function () {
-        Route::get('/', [\App\Http\Controllers\EmployeeController::class, 'getAll']);
-        Route::post('/', [\App\Http\Controllers\EmployeeController::class, 'create']);
-        Route::post('/{employee}', [\App\Http\Controllers\EmployeeController::class, 'update']);
-        Route::delete('/{employee}', [\App\Http\Controllers\EmployeeController::class, 'delete']);
+    Route::prefix("employees")->group(function () {
+        Route::get("/", [
+            \App\Http\Controllers\EmployeeController::class,
+            "getAll",
+        ]);
+        Route::post("/", [
+            \App\Http\Controllers\EmployeeController::class,
+            "create",
+        ]);
+        Route::post("/{employee}", [
+            \App\Http\Controllers\EmployeeController::class,
+            "update",
+        ]);
+        Route::delete("/{employee}", [
+            \App\Http\Controllers\EmployeeController::class,
+            "delete",
+        ]);
     });
 });
 
-Route::get('nilaiRT', [\App\Http\Controllers\NilaiController::class, 'nilaiRT']);
-Route::get('nilaiST', [\App\Http\Controllers\NilaiController::class, 'nilaiST']);
+Route::get("nilaiRT", [
+    \App\Http\Controllers\NilaiController::class,
+    "nilaiRT",
+]);
+Route::get("nilaiST", [
+    \App\Http\Controllers\NilaiController::class,
+    "nilaiST",
+]);
 
-Route::fallback(function () {
-    return response()->json([
-        'status' => 'error',
-        'message' => 'route not found',
-    ], Response::HTTP_NOT_FOUND);
-});
+// Route::fallback(function () {
+//     return response()->json([
+//         'status' => 'error',
+//         'message' => 'route not found',
+//     ], Response::HTTP_NOT_FOUND);
+// });
